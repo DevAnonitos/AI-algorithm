9f67fa878e1b54b6a695baec8e720c0b871ab296
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-07-07 21:54:41 +0700

    update-AddHandleImage

--- DL/OpenCV/Module1/1.image.py
+++ DL/OpenCV/Module1/1.image.py
@@ -1,1 +1,16 @@
+import cv2
 
+img = cv2.imread('D:/2-bai tap cua dev/PythonAI/AI/DL/OpenCV/Module1/img/img1.jpg')
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+if img is None:
+    print('Can not read this file')
+else:
+    cv2.imshow('Image', img)
+    cv2.imwrite('D:/2-bai tap cua dev/PythonAI/AI/DL/OpenCV/Module1/img/img1.png', img)
+
+    cv2.imshow("GrayScaleImage", gray_img)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
